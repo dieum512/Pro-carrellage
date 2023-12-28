06bfeb1b4ad338b92c1506138a7c4cefbeb9b4a1
dieum512 <akonkwadieum222@gmail.com>
2023-12-28 21:22:06 +0200

    Update the App.js file with the compoents

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React from 'react'
+import Home from './components/Home/Home'
+import About from './components/About/About'
+import Difference from './components/Difference/Difference'
+import Testimonials from './components/Testimonials/Testimonials'
+import Projects from './components/Projects/Projects'
+import Contact from './components/Contact/Contact'
+import Footer from './components/Footer/Footer'
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      < Home />
+      < About />
+      < Difference />
+      < Testimonials />
+      < Projects />
+      < Contact />
+      < Footer />
+    </>
+  )
 }
 
-export default App;
+export default App
