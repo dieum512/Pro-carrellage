057bc4b236e3e055b6f5d30090c9730b6de745e2
dieum512 <akonkwadieum222@gmail.com>
2023-12-29 22:30:02 +0200

    Update the homepage

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,7 +2,15 @@ import React from 'react';
 import './home.css';
 
 const Home = () => (
-  <div>Home</div>
+  <div className="container">
+    <div className="home__cont">
+      <div className="info__cont">
+        <h1>Carrelleur professionel</h1>
+        <h3>Des professionnels du carrelage dévoués à embellir votre espace avec précision et expertise, pour un intérieur qui respire l'élégance et le savoir-faire</h3>
+        <a href="#contact">Contactez nous</a>
+      </div>
+    </div>
+  </div>
 );
 
 export default Home;
