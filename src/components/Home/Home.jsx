fddb6bff4667e5f21ae7f1d4c836105acfcb8a9d
dieum512 <akonkwadieum222@gmail.com>
2023-12-30 21:02:10 +0200

    Update the button

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { VscSend } from "react-icons/vsc"
 import './home.css';
 
 const Home = () => (
@@ -7,7 +8,7 @@ const Home = () => (
       <div className="info__cont">
         <h1>Carrelleur professionel</h1>
         <h3>Des professionnels du carrelage dévoués à embellir votre espace avec précision et expertise, pour un intérieur qui respire l'élégance et le savoir-faire</h3>
-        <a href="#contact">Contactez nous</a>
+        <a href="#contact"><span>Contactez nous</span><span className='send'><VscSend/></span></a>
       </div>
     </div>
   </div>
