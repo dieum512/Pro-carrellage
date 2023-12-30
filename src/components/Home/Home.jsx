4bb0eedc6b5b2338f1de3e94bf608915589b9497
dieum512 <akonkwadieum222@gmail.com>
2023-12-30 21:11:30 +0200

    Fix the jsx linter error

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { VscSend } from "react-icons/vsc"
+import { VscSend } from 'react-icons/vsc';
 import './home.css';
 
 const Home = () => (
@@ -7,8 +7,14 @@ const Home = () => (
     <div className="home__cont">
       <div className="info__cont">
         <h1>Carrelleur professionel</h1>
-        <h3>Des professionnels du carrelage dévoués à embellir votre espace avec précision et expertise, pour un intérieur qui respire l'élégance et le savoir-faire</h3>
-        <a href="#contact"><span>Contactez nous</span><span className='send'><VscSend/></span></a>
+        <h3>
+          Des professionnels du carrelage dévoués à embellir votre espace avec précision
+          et expertise, pour un intérieur qui respire l&rsquo;élégance et le savoir-faire
+        </h3>
+        <a href="#contact">
+          <span>Contactez nous</span>
+          <span className="send"><VscSend /></span>
+        </a>
       </div>
     </div>
   </div>
