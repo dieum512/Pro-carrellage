fd096c7a63f718166b88b6dea84ba1addb6351d6
dieum512 <akonkwadieum222@gmail.com>
2023-12-31 10:42:09 +0200

    Fix the jsx linter errors

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,20 +4,24 @@ import './about.css';
 const About = () => (
   <>
     <div className="about__container">
-        <h2>Qui <span> nous sommes</span></h2>
-        <p>
-            Une entreprise de construction basé sur le finissage. 
-            Nous offrons à nos clients un large choix de services, 
-            quels que soient leurs besoins. Peu importe votre projet, 
-            nos jeunes spécialistes sont là pour concrétiser vos désirs. 
-            Pour un devis gratuit, contactez-nous
-        </p>
-        <p className='second__p'>
-            Depuis son entrée dans le secteur en 2018 Procarrellage est réputé(e) être un(e) Société de construction 
-            parmi les plus fiables de GOMA. Nous offrons à nos clients un large choix de services, 
-            quels que soient leurs besoins. Peu importe votre projet, 
-            nos spécialistes sont là pour concrétiser vos désirs.
-        </p>
+      <h2>
+        Qui
+        <span> nous sommes</span>
+      </h2>
+      <p>
+        Une entreprise de construction basé sur le finissage.
+        Nous offrons à nos clients un large choix de services,
+        quels que soient leurs besoins. Peu importe votre projet,
+        nos jeunes spécialistes sont là pour concrétiser vos désirs.
+        Pour un devis gratuit, contactez-nous
+      </p>
+      <p className="second__p">
+        Depuis son entrée dans le secteur en 2018 Procarrellage est réputé être un
+        Société de construction
+        parmi les plus fiables de GOMA. Nous offrons à nos clients un large choix de services,
+        quels que soient leurs besoins. Peu importe votre projet,
+        nos spécialistes sont là pour concrétiser vos désirs.
+      </p>
     </div>
   </>
 );
