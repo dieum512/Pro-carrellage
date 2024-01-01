e4a8328db8b64492abbe9607e02910a1de2d0a56
dieum512 <akonkwadieum222@gmail.com>
2024-01-01 20:44:15 +0200

    Fix the JSX linter errors

--- src/components/Difference/Difference.jsx
+++ src/components/Difference/Difference.jsx
@@ -4,17 +4,17 @@ import './difference.css';
 const Difference = () => (
   <>
     <div className="diff__cont">
-        <h2>
-            Qu'est-ce qui nous rend
-            {' '}
-            <span>différents ?</span>
-        </h2>
-        <p>
-            Chez Procarrellage, nos clients sont notre priorité 
-            et nous mettons tout en œuvre pour les satisfaire pleinement. 
-            Grâce à ce service, nous sommes en mesure de mener les projets 
-            les plus complexes et de garantir un travail d'excellente qualité
-        </p>
+      <h2>
+        Qu&apos;est-ce qui nous rend
+        {' '}
+        <span>différents ?</span>
+      </h2>
+      <p>
+        Chez Procarrellage, nos clients sont notre priorité
+        et nous mettons tout en œuvre pour les satisfaire pleinement.
+        Grâce à ce service, nous sommes en mesure de mener les projets
+        les plus complexes et de garantir un travail d&apos;excellente qualité
+      </p>
     </div>
   </>
 );
